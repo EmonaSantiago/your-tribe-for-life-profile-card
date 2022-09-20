a05f649284889864d7b8ed7c25b2b1ce2c5e5933
Emona Santiago <emonasantiago1207@gmail.com>
2022-09-20 23:38:30 +0200

    css
    still fixing stylings

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,20 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import "../styles/global.css"
+</script>
+
+<main class="visite-book">
+    <article class="front-book">
+        <section class="title">
+            <!-- prismic insert data -->
+            <h1>Emona Santiago</h1>
+        </section>
+
+        <section class="part">
+            <!-- prismic data -->
+            <h2>Het visitekaartje</h2>
+            <h3>Sprint 13</h3>
+        </section>
+    </article>
+
+    <article class="firstpage-book">
+        <section class="title">
+            <!-- prismic insert data -->
+            <h1>Emona Santiago</h1>
+        </section>
+
+        <section class="intro">
+            <!-- prismic data -->
+            <p>Mijn naam is Emona en ik ben 18 jaar oud. Ik ben filipijnse afkomst en woon in Amsterdam</p>
+        </section>
+
+        <section class="image">
+            <!-- prismic data -->
+            <img src="" alt="">
+        </section>
+    </article>
+
+    <article class="secondpage-book">
+        <section class="content">
+            <h2>Hobbies</h2>
+            <p>Ik dans en teken graag. Ook speel ik de Gitaar, Ukulele en Piano</p>
+
+            <h2>Studie</h2>
+            <p>Nu zit ik het tweede jaar van Frontend Design and Development</p>
+
+            <a href="url">Instagram</a>
+            <a href="url">Github</a>
+        </section>
+    </article>
+    
+</main>
 
 
-<h1>Emona Santiago</h1>
-<h2>Het visitekaartje</h2>
-<h3>Sprint 13</h3>
 
-<h1>Emona Santiago</h1>
-<p>Mijn naam is Emona en ik ben 18 jaar oud. Ik ben filipijnse afkomst en woon in Amsterdam</p>
-<img src="" alt="">
 
-<h2>Hobbies</h2>
-<p>Ik dans en teken graag. Ook speel ik de Gitaar, Ukulele en Piano</p>
-
-<h2>Studie</h2>
-<p>Nu zit ik het tweede jaar van Frontend Design and Development</p>
-
-<a href="url">Instagram</a>
-<a href="url">Github</a>
