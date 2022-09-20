503d910da646826de83efc3ad995cd81cdc3da50
Emona Santiago <emonasantiago1207@gmail.com>
2022-09-20 21:38:28 +0200

    change

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<h1>Emona Santiago</h1>
+<h2>Het visitekaartje</h2>
+<h3>Sprint 13</h3>
+
+<h1>Emona Santiago</h1>
+<p>Mijn naam is Emona en ik ben 18 jaar oud. Ik ben filipijnse afkomst en woon in Amsterdam</p>
+<img src="" alt="">
+
+<h2>Hobbies</h2>
+<p>Ik dans en teken graag. Ook speel ik de Gitaar, Ukulele en Piano</p>
+
+<h2>Studie</h2>
+<p>Nu zit ik het tweede jaar van Frontend Design and Development</p>
+
+<a href="url">Instagram</a>
+<a href="url">Github</a>
