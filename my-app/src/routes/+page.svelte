95bd250f696e1815ca9ffb9663092651103a9664
Emona Santiago <emonasantiago1207@gmail.com>
2022-09-23 02:25:08 +0200

    styling

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,54 +1,26 @@
-
 <script>
-    import "../styles/global.css"
+
+  import { dataset_dev } from "svelte/internal";
+  import "../styles/global.css"
+  const imgUrl = new URL('', import.meta.url).href
+  
 </script>
 
-<main class="visite-book">
-    <article class="front-book">
-        <section class="title">
-            <!-- prismic insert data -->
-            <h1>Emona Santiago</h1>
-        </section>
-
-        <section class="part">
-            <!-- prismic data -->
-            <h2>Het visitekaartje</h2>
-            <h3>Sprint 13</h3>
-        </section>
-    </article>
-
-    <article class="firstpage-book">
-        <section class="title">
-            <!-- prismic insert data -->
-            <h1>Emona Santiago</h1>
-        </section>
-
-        <section class="intro">
-            <!-- prismic data -->
-            <p>Mijn naam is Emona en ik ben 18 jaar oud. Ik ben filipijnse afkomst en woon in Amsterdam</p>
-        </section>
-
-        <section class="image">
-            <!-- prismic data -->
-            <img src="" alt="">
-        </section>
-    </article>
-
-    <article class="secondpage-book">
-        <section class="content">
-            <h2>Hobbies</h2>
-            <p>Ik dans en teken graag. Ook speel ik de Gitaar, Ukulele en Piano</p>
-
-            <h2>Studie</h2>
-            <p>Nu zit ik het tweede jaar van Frontend Design and Development</p>
-
-            <a href="url">Instagram</a>
-            <a href="url">Github</a>
-        </section>
-    </article>
-    
-</main>
-
-
-
-
+<section class="Profile-Card">
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <h2>Emona Santiago</h2>
+                <img class="profile-picture" src="{imgUrl}" alt="afbeelding van emona">
+            </div>
+            <div class="flip-card-back">
+              <h1>Emona Santiago</h1> 
+              <p>Github Link</p> 
+              <p>Bio</p>
+              <p>2de jaars FDND</p>
+              <p>Mijn naam is Emona en ik ben 18 jaar oud. Ik ben Filipijnse afkomst en woon in Amsterdam.</p>
+              <p></p>
+            </div>
+          </div>
+    </div>
+</section>
